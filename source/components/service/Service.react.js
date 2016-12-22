57bda4a33305b009bc23c0d11ad8d1413d0eff3a
张帆 <fireworks_zf@163.com>
2016-12-22 11:31:26 +0800

    service

--- source/components/service/Service.react.js
+++ source/components/service/Service.react.js
@@ -1,26 +1,28 @@
 var React = require('react');
 
+require('./service.css');
+
 var Service = React.createClass({
 
     render: function() {
         return (
             <div id="service" className="service">
                 <div className="wrap">
-                    <a className="service-item-helper">
+                    <a className="service-item-helper" href="">
                         <i></i>
-                        <em></em>
+                        <em>摇号助手</em>
                     </a>
-                    <a className="service-item-product">
+                    <a className="service-item-product" href="">
                         <i></i>
-                        <em></em>
+                        <em>车型大全</em>
                     </a>
-                    <a className="service-item-test">
+                    <a className="service-item-test" href="">
                         <i></i>
-                        <em></em>
+                        <em>免费试驾</em>
                     </a>
-                    <a className="service-item-mall">
+                    <a className="service-item-mall" href="">
                         <i></i>
-                        <em></em>
+                        <em>限时特惠</em>
                     </a>
                 </div>
             </div>
